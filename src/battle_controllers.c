c2c35e64685ac40d82ae44d266541d73302cbf50
oblskkami <primal000@163.com>
2026-08-01 16:04:03 +0800

    追逐小田卷的蛇纹熊换成土狼犬

--- src/battle_controllers.c
+++ src/battle_controllers.c
@@ -152,7 +152,7 @@ void SetUpBattleVarsAndBirchZigzagoon(void)
     BattleAI_SetupFlags();
 
     if (!IS_FRLG && gBattleTypeFlags & BATTLE_TYPE_FIRST_BATTLE)
-        CreateWildMon(SPECIES_ZIGZAGOON, 2);
+        CreateWildMon(SPECIES_POOCHYENA, 2);
 }
 
 void InitBattleControllers(void)
