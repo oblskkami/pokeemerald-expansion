0f344a409b861ebc9c024af4f6691b0d71d8ddd4
oblskkami <primal000@163.com>
2026-09-04 11:28:10 +0800

    满充战斗bgm调整

--- src/save_dialog.c
+++ src/save_dialog.c
@@ -363,7 +363,7 @@ static u8 SaveConfirmSaveCallback(void)
 {
     ShowSaveInfoWindow();
 
-    if (InBattlePyramid_()) {
+    if (InBattlePyramid()) {
         ShowSaveMessage(gText_BattlePyramidConfirmRest, SaveYesNoCallback);
     }
     else {
